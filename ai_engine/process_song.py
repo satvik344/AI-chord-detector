ae26390249c3e835c7bda50a0a99e2bb6b3f7ff7
SATVIK <satvik344705@gmail.com>
2026-05-26 22:43:00 +0530

    Removed demucs for production deployment

--- ai_engine/process_song.py
+++ ai_engine/process_song.py
@@ -1,12 +1,12 @@
 from ai_engine.chord_engine import detect_chords
 
 
-def process_song(file_path):
+def process_song(audio_path):
 
     print("\nSTEP 1: Detecting chords directly...\n")
 
-    chords = detect_chords(
-        file_path
-    )
+    chords = detect_chords(audio_path)
+
+    print("\nDONE!\n")
 
     return chords
